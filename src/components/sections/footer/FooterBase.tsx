2e48fdb34e17e5b978f8edfdab6e20298098c794
bender <kudin.dima@gmail.com>
2026-03-05 18:26:38 +0000

    Switch to version 2: modified src/components/sections/footer/FooterBase.tsx

--- src/components/sections/footer/FooterBase.tsx
+++ src/components/sections/footer/FooterBase.tsx
@@ -30,11 +30,11 @@ interface FooterBaseProps {
 
 const FooterBase = ({
   // logoSrc = "/brand/logowhite.svg",
-  logoText = "Contact Us ",
+  logoText = "Webild",
   // logoWidth = 120,
   // logoHeight = 40,
   columns,
-  copyrightText = `© 2025 | Contact Us `,
+  copyrightText = `© 2025 | Webild`,
   onPrivacyClick,
   ariaLabel = "Site footer",
   className = "",
